6473f0f3dc731803437f395a78773e17f81614d3
sik-sak <siksak2024@gmail.com>
2024-09-05 11:56:01 +0530

    Fixed time delta

--- Server/Stock_Data/realtimeFetch.py
+++ Server/Stock_Data/realtimeFetch.py
@@ -77,7 +77,7 @@ def fetch_and_update_data():
             pass
         elif fetch_time.time() <= now_ist.time() <= market_close.time():
             end_time = now_ist
-            data = nifty50.history(interval="1m", start=datetime.datetime.strptime(last_run, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(minutes=1), end=end_time)
+            data = nifty50.history(interval="1m", start=datetime.datetime.strptime(last_run, "%Y-%m-%d %H:%M:%S") - datetime.timedelta(minutes=2), end=end_time)
             logging.info(f"Timestamp - {end_time.strftime('%Y-%m-%d %H:%M:%S')} - Data fetched from {last_run} to {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
             jsonData['last_updated_at'] = end_time.strftime('%Y-%m-%d %H:%M:%S')
         else:
